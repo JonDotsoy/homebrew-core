e8b6d213d1f823607bec43594e32a7de73a73169
Jonathan Delgado <JonDotsoy@users.noreply.github.com>
2023-12-27 15:36:57 -0300

    chore: uid v1.3.0

--- uid.rb
+++ uid.rb
@@ -4,14 +4,15 @@
 class Uid < Formula
   desc "Unique ID generator"
   homepage "https://github.com/JonDotsoy/uid"
-  url "https://api.github.com/repos/JonDotsoy/uid/tarball/v1.2.0"
-  sha256 "2a80347c06d1b124294d236e78e5f790eb1eada25b1a2e5e3b157e27ea6cf14f"
+  url "https://api.github.com/repos/JonDotsoy/uid/tarball/v1.3.0"
+  sha256 "a49e08f59c3d8f84b45d49f3d4c0f08b80c78fa7c960191478fff52b8dc2b6fe"
   license "MIT"
 
-  depends_on "deno"
+  depends_on "bun"
 
   def install
-    system "deno","compile","-o","uid","uid.ts"
+    system "bun", "install"
+    system "bun", "build","--compile","--outfile","uid","uid.ts"
     bin.install "uid" => "uid"
   end
 
