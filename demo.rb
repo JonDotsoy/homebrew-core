7abdb7248c98b9d930ac3e7189feed453643863c
Jonathan Delgado <hi@jon.soy>
2024-01-27 20:39:21 -0300

    refactor: Rename and rebase `Ndate` class to `Demo`
    This update contains the renaming of Ndate class to Demo, also
    reflecting this migration in the Formula desc and homepage url. The
    rationale for the rebase and renaming operation lies in aligning the
    application class name and its repository url, ensuring better
    consistency and clarity for future usage and maintenance.
    
    No significant consequences are expected from this change in terms of
    application functionality. This is purely a naming and structural
    correction for better alignment with the hosted repository.

--- demo.rb
+++ demo.rb
@@ -1,6 +1,6 @@
-class Ndate < Formula
+class Demo < Formula
   desc "Date format to console"
-  homepage "https://github.com/JonDotsoy/ndate/tree/morgan-v0.4.1#readme"
+  homepage "https://github.com/JonDotsoy/demo/tree/v1.0.0#readme"
   url "https://api.github.com/repos/JonDotsoy/demo/tarball/v1.0.0"
   sha256 "f02f7aa6f6002991c21e972fec049d910330c6b9de263ab8f380c93d99e1e710"
   license "MIT"
