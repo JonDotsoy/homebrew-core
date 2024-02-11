2c319aee122aabccffb4fee73b8786a332fd4e59
Jonathan Delgado <hi@jon.soy>
2024-02-10 23:06:53 -0300

    feat: add Homebrew formula for GitAssistant and update package.json with scan script
    This commit introduces a Homebrew formula for GitAssistant, a tool designed to help generate commit messages with AI support. The formula specifies the GitAssistant version, its dependencies, and the installation process. Additionally, this commit updates package.json by adding a new script named "scan", which utilizes `bun` for executing a TypeScript scanning script. These changes aim to enhance the development workflow by simplifying the installation of GitAssistant for macOS users and providing an automated scanning feature for project analysis.

---
+++ git-assistant.rb
@@ -0,0 +1,35 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class GitAssistant < Formula
+    desc "Make messages with AI. (use `git assistant init` in your project)"
+    homepage "https://github.com/JonDotsoy/commands-help/tree/develop/commands/git-tools#readme"
+    url "https://api.github.com/repos/jondotsoy/commands-help/tarball/git-assistant-v0.2.0"
+    version "v0.2.0"
+    sha256 "f435ea41a5ae45448beff80d5abff0181f7947231be92a4b4bfc15a434c0603d"
+    license "MIT"
+  
+    depends_on "bun"
+    depends_on "make"
+  
+    def install
+      cd "commands/git-assistant" do
+        system "make", "install"
+        system "make", "build"
+        bin.install "dist/git-assistant" => "git-assistant"
+      end
+    end
+  
+    # test do
+    #   # `test do` will create, run in and delete a temporary directory.
+    #   #
+    #   # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    #   # this will need to be a test that verifies the functionality of the
+    #   # software. Run the test with `brew test git-assistant`. Options passed
+    #   # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #   #
+    #   # The installed folder is not in the path, so use the entire path to any
+    #   # executables being tested: `system "#{bin}/program", "do", "something"`.
+    #   # system "false"
+    # end
+end
