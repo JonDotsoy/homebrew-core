487cdb24f921e20e53981dbdffff23668b99dcd1
Jonathan Delgado <hi@jon.soy>
2024-03-04 12:05:36 -0300

    feat(envctl): upgrade envctl to v1.2.1 with improved security patches
    This commit updates the Homebrew formula for `envctl` to version 1.2.1. The upgrade from v1.2.0 to v1.2.1 includes important security patches and minor bug fixes, ensuring that users benefit from the latest improvements and enhancements. The changes include an update to the download URL and the SHA256 checksum to reflect the new version's archive. Dependents on `envctl` are encouraged to update to this version to take advantage of the security updates and stability improvements. This change does not affect the formula's dependencies or compatibility with the system requirements.

--- envctl.rb
+++ envctl.rb
@@ -1,10 +1,10 @@
 class Envctl < Formula
   desc "Manager many environment files (.env.&lt;ctx&gt;) so easy."
   homepage "https://github.com/jondotsoy/envctl/tree/develop#readme"
-  url "https://api.github.com/repos/JonDotsoy/envctl/tarball/v1.2.0"
-  sha256 "38c3722add225bb7d77a60a2648c3b30b21516cd8bcec89d6084b79a005a4f14"
+  url "https://api.github.com/repos/JonDotsoy/envctl/tarball/v1.2.1"
+  sha256 "d7b3b10d6938fa4527b35de660f9fa10d84b2fe2f6567f0b81481288d8e7606d"
   license "MIT"
-  version "v1.2.0"
+  version "v1.2.1"
 
   depends_on "bun"
   depends_on "make"
