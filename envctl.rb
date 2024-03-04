abb847f249c274c323981807723c5589b4d0b77d
Jonathan Delgado <hi@jon.soy>
2024-03-03 22:30:03 -0300

    feat(envctl): upgrade envctl to version v1.0.1
    This commit updates the envctl formula to the new version v1.0.1. The update includes changes to the download URL and SHA256 checksum to ensure the integrity and authenticity of the downloaded package. This version upgrade may include bug fixes, performance improvements, and new features that enhance the user experience with envctl. Users are encouraged to upgrade to take advantage of these improvements.

--- envctl.rb
+++ envctl.rb
@@ -1,10 +1,10 @@
 class Envctl < Formula
   desc "Manager many environment files (.env.&lt;ctx&gt;) so easy."
   homepage "https://github.com/jondotsoy/envctl/tree/develop#readme"
-  url "https://api.github.com/repos/JonDotsoy/envctl/tarball/v1.0.0"
-  sha256 "a800a55dc87e7ec3fab7bb25cbc80f27c4ebaac4e71e5774bedcfd25272bf7a2"
+  url "https://api.github.com/repos/JonDotsoy/envctl/tarball/v1.0.1"
+  sha256 "8695406ae1d17b292ea3e284dadb56aea7136187f7dc7a58ab1d631cdb1a6f7f"
   license "MIT"
-  version "v1.0.0"
+  version "v1.0.1"
 
   depends_on "bun"
   depends_on "make"
