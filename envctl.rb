651f34d4689537e6372389687c44c90dd680e84f
Jonathan Delgado <hi@jon.soy>
2024-03-04 09:49:22 -0300

    feat(envctl): Update envctl to v1.2.0
    Updated the `envctl` formula to point to the latest release v1.2.0. This update includes the new release tarball URL and updates the SHA256 checksum to match the new tarball. With this change, users will be able to install the latest features and improvements included in `envctl` v1.2.0. The dependency on "bun" and "make" remains unchanged, ensuring backward compatibility and stability of the environment management tool.

--- envctl.rb
+++ envctl.rb
@@ -1,10 +1,10 @@
 class Envctl < Formula
   desc "Manager many environment files (.env.&lt;ctx&gt;) so easy."
   homepage "https://github.com/jondotsoy/envctl/tree/develop#readme"
-  url "https://api.github.com/repos/JonDotsoy/envctl/tarball/v1.1.0"
-  sha256 "1aa30f04bc15f08f015f4f7b0bc760eb9edac1b62ae26b7b6008eb2a96240e27"
+  url "https://api.github.com/repos/JonDotsoy/envctl/tarball/v1.2.0"
+  sha256 "38c3722add225bb7d77a60a2648c3b30b21516cd8bcec89d6084b79a005a4f14"
   license "MIT"
-  version "v1.1.0"
+  version "v1.2.0"
 
   depends_on "bun"
   depends_on "make"
