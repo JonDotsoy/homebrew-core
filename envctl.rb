14ae6f8d3303540b0cfc4c0a62c7e369890eede5
Jonathan Delgado <hi@jon.soy>
2024-03-12 16:23:59 -0300

    feat(envctl): Update envctl to v1.3.0 in Homebrew formula
    This commit updates the `envctl` Homebrew formula to version 1.3.0. The upgrade includes changing the download URL and the SHA256 checksum to match the new release. This update ensures users get the latest features and bug fixes of `envctl`. The dependency on `oven-sh/bun/bun` and `make` remains unchanged. This version bump is a part of the routine maintenance to keep software versions up to date in the Homebrew repository.

--- envctl.rb
+++ envctl.rb
@@ -1,10 +1,10 @@
 class Envctl < Formula
   desc "Manager many environment files (.env.&lt;ctx&gt;) so easy."
   homepage "https://github.com/jondotsoy/envctl/tree/develop#readme"
-  url "https://api.github.com/repos/JonDotsoy/envctl/tarball/v1.2.1"
-  sha256 "d7b3b10d6938fa4527b35de660f9fa10d84b2fe2f6567f0b81481288d8e7606d"
+  url "https://api.github.com/repos/JonDotsoy/envctl/tarball/v1.3.0"
+  sha256 "14f3ac341cf942981eeb07b91aeae671343f22a7bbfdb9ee5ce73af99c3d4765"
   license "MIT"
-  version "v1.2.1"
+  version "v1.3.0"
 
   depends_on "oven-sh/bun/bun"
   depends_on "make"
