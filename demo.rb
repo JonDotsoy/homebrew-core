bc3aa2cfec1fe0f17b6fe3b4ccb9ffa726a20a15
Jonathan Delgado <hi@jon.soy>
2024-06-23 22:42:45 -0400

    fix(demo): upgrade depencies

--- demo.rb
+++ demo.rb
@@ -5,7 +5,7 @@ class Demo < Formula
   sha256 "1569fc512320b38cd360c8dc84effb0350dd91b83a3d1e99461986ade19a7f34"
   license "MIT"
 
-  depends_on "bun"
+  depends_on "oven-sh/bun/bun"
 
   def install
     system "bun", "install"
