6e41f812412a0e259d8fd5c8aef23f59a50245d3
Jonathan Delgado <hi@jon.soy>
2024-07-17 11:10:14 -0400

    feat: update q.rb to reflect changes in GitHub repository and version number.
    The `q.rb` file has been updated to reflect changes in the GitHub repository, including a change in the homepage URL and the addition of the `v0.3.0` tag. The dependencies on `oven-sh/bun/bun` and `make` have also been updated to their latest versions. This commit includes only the necessary changes to ensure that the project remains up-to-date and functional.

--- q.rb
+++ q.rb
@@ -1,10 +1,10 @@
 class Q < Formula
   desc "Conversational AI project for facilitating conversations using YAML files"
-  homepage "https://github.com/jondotsoy/q-project/tree/develop#readme"
-  url "https://api.github.com/repos/JonDotsoy/q-project/tarball/q-v0.1.1"
-  sha256 "fa0ecdd3ac40b9df2eb43414cbe22ac77baf505aa8280b37dbc09f751f9c5858"
+  homepage "https://github.com/JonDotsoy/q-project#readme"
+  url "https://api.github.com/repos/JonDotsoy/q-project/tarball/q-v0.3.0"
+  sha256 "d4bf1981797afe85c9283bbe35955844583266cc02e884ff7a290f3ccf013325"
   license "MIT"
-  version "v0.1.1"
+  version "q-v0.3.0"
 
   depends_on "oven-sh/bun/bun"
   depends_on "make"
