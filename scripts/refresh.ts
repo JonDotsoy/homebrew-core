7748f040cc39cb1cb3b2e2f4c5e3621d4166e53f
Jonathan Delgado <hi@jon.soy>
2024-07-19 20:02:54 -0400

    build: upgrade refresh script

--- scripts/refresh.ts
+++ scripts/refresh.ts
@@ -1,9 +1,17 @@
 #!/usr/bin/env bun
 
 import { GhApi } from "./utils/gh-api";
-import * as fs from "fs/promises";
 import Handlebars from "handlebars";
 import { digestFile } from "./utils/digest-file-url";
+
+
+const setOutput = (name: string, variable: string) => {
+    const GITHUB_OUTPUT = process.env.GITHUB_OUTPUT;
+
+    if (GITHUB_OUTPUT) {
+        Bun.file(GITHUB_OUTPUT).writer().write(`${name}=${variable}\n`);
+    }
+}
 
 const handlebars = Handlebars.create()
 
@@ -12,8 +20,8 @@ const [_execPath, _scriptPath, repoName] = process.argv;
 const configPath = new URL(`./templates/${repoName}.json`, import.meta.url);
 const templatePath = new URL(`./templates/${repoName}.template`, import.meta.url);
 
-const template = await fs.readFile(templatePath, 'utf-8');
-const config = JSON.parse(await fs.readFile(configPath, 'utf-8'));
+const template = await Bun.file(templatePath).text();
+const config = JSON.parse(await Bun.file(configPath).text());
 
 const ghApi = new GhApi({ cache: false });
 
@@ -21,7 +29,18 @@ const repo = await ghApi.run('api', `/repos/${repoName}`).json()
 const release = await ghApi.run('api', `/repos/${repoName}/releases/latest`).json()
 const digest = await digestFile(new URL(release.tarball_url));
 const output = new URL(config.output, configPath)
-const render =  handlebars.compile(template, { noEscape: true })({ digest, config, repo, release })
+const render = handlebars.compile(template, { noEscape: true })({ digest, config, repo, release })
 
-console.log(`Write file ${output}`)
-await fs.writeFile(output, render);
+const hexFromArrayBuffer = (arrayBuffer: ArrayBuffer) => Array.from(new Uint8Array(arrayBuffer), e => e.toString(16).padEnd(2, '0')).join('')
+
+const renderHash = hexFromArrayBuffer(await crypto.subtle.digest('sha1', new TextEncoder().encode(render)));
+const outputHash = hexFromArrayBuffer(await crypto.subtle.digest('sha1', await Bun.file(output).arrayBuffer()));
+
+const updated = renderHash !== outputHash
+
+if (updated) {
+    console.log(`Write file ${output}`)
+    Bun.file(output).writer().write(render);
+}
+
+setOutput('updated', `${updated}`);
