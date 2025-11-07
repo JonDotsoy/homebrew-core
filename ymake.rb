9033a081f72bf237cd8ba2b407c325598b80dbe8
Jonathan Delgado <hi@jon.soy>
2025-11-07 00:00:17 -0300

    Update ymake to version 1.0.4

--- ymake.rb
+++ ymake.rb
@@ -1,9 +1,9 @@
 class Ymake < Formula
   desc "Un ejecutor de tareas similar a Make pero con archivos YAML, con características modernas como builds matriciales y outputs estructurados"
   homepage "https://www.npmjs.com/package/@jondotsoy/ymake?activeTab=readme"
-  url "https://registry.npmjs.org/@jondotsoy/ymake/-/ymake-1.0.2.tgz"
-  sha256 "347c3ac9aaf97b85097642b0a3be07ad7453cac586bdfbd0abdba2e233039029"
-  version "1.0.2"
+  url "https://registry.npmjs.org/@jondotsoy/ymake/-/ymake-1.0.4.tgz"
+  sha256 "f75308a0e6faec17cc8cf9ed1d575e9f58afaee0da53cf488175834a8799aba8"
+  version "1.0.4"
 
   depends_on "oven-sh/bun/bun"
 
