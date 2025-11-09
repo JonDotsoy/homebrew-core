5c1880fe41c6d1c7a757ebe9e7eab8fd89ec68c1
Jonathan Delgado <hi@jon.soy>
2025-11-09 14:40:56 -0300

    Update ymake to version 1.5.3

--- ymake.rb
+++ ymake.rb
@@ -1,9 +1,9 @@
 class Ymake < Formula
   desc "Un ejecutor de tareas similar a Make pero con archivos YAML, con características modernas como builds matriciales y outputs estructurados"
   homepage "https://www.npmjs.com/package/@jondotsoy/ymake?activeTab=readme"
-  url "https://registry.npmjs.org/@jondotsoy/ymake/-/ymake-1.5.2.tgz"
-  sha256 "c7194ec1b980ee7cb042da84424ba0ae2e3209c5f23ebf61fdb08abece89b125"
-  version "1.5.2"
+  url "https://registry.npmjs.org/@jondotsoy/ymake/-/ymake-1.5.3.tgz"
+  sha256 "9b361251b586d3775a98b0dc533de15ec48db9b7933a4e4719c93a5188b9fea2"
+  version "1.5.3"
 
   depends_on "oven-sh/bun/bun"
 
